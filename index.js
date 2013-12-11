8dc32189c4b5f349d616ba46c15a356bc1129a3f
Rogerz Zhang <rogerz.zhang@gmail.com>
2013-12-11 20:08:43 +0800

    Use templates so it could be included in installation

--- index.js
+++ index.js
@@ -6,16 +6,19 @@ var mime = require('mime');
 
 module.exports = function (builder) {
   builder.hook('before scripts', function(pkg, callback){
-    if (!pkg.config.dataURIs) {
+    if (!pkg.config.templates) {
       return callback();
     }
-    pkg.config.dataURIs.forEach(function (file) {
+    pkg.config.templates.filter(function (file) {
+      return /\.(png|jpg|bmp)$/.test(file);
+    }).forEach(function (file) {
       var fullPath = pkg.path(file);
 
       // http://nodeexamples.com/2012/09/26/base-64-encoding-images-in-node-js/
       var data = fs.readFileSync(fullPath).toString('base64');
       var converted = util.format('"data:%s;base64,%s"', mime.lookup(file), data);
 
+      pkg.removeFile('templates', file);
       pkg.addFile('scripts', file + '.js', 'module.exports = ' + converted);
     });
     return callback();
